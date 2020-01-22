6dba8418c4f4148d73a29fe3b6a02a768614d8c3
jiminjeong22 <jiminjeong22@users.noreply.github.com>
2020-01-22 23:51:40 +0900

    20200122

--- Mood Disorder Diagnosis Chatbot/v_1 (openpyxl_use)/main.py
+++ Mood Disorder Diagnosis Chatbot/v_1 (openpyxl_use)/main.py
@@ -1,6 +1,3 @@
-import random
-import emoji
-
 from flask import Flask, request, make_response, jsonify
 
 import threading
@@ -56,7 +53,7 @@ def webhook():
 
         # context adding
         context_data.append((text, mood))
-        context_n = len(context_data)
+        context_n = len(context_data) - 1
 
         if len(context_data) < 4:
             question = "mood"
@@ -69,35 +66,9 @@ def webhook():
         elif len(context_data) == 5:
             question = "eat"
             res, eating_disorder = respon.sleep_response(text, morph_l)
-
         else:
-            if mood < -1.5:  # 강한 부정
-                res_list = [
-                    emoji.emojize("맛있는 음식을 먹으면 기분이 좀 나아지지 않을까요? :fork_and_knife: :spaghetti: :sushi:",
-                                  use_aliases=True),
-                    emoji.emojize("우리 신나는 음악 들어볼래요? 기분이 나아질 거에요", use_aliases=True),
-                    emoji.emojize("무엇을 하면 기분이 좋아질 수 있을까요? ", use_aliases=True),
-                    emoji.emojize("재미있는 영화를 보면 기분이 나아질까요? ", use_aliases=True)
-                ]
-
-                res_num = random.randrange(0, len(res_list))
-                res = res_list[res_num]
-            elif mood > 1.5:  # 강한 긍정
-                res_list = [
-                    emoji.emojize("오늘 하루를 더 기분좋은 하루로 만들어봐요 우리!", use_aliases=True),
-                    emoji.emojize("내일도 오늘처럼 기분 좋은 하루가 되길 바래요", use_aliases=True),
-                ]
-
-                res_num = random.randrange(0, len(res_list))
-                res = res_list[res_num]
-            else:  # 중성
-                res_list = [
-                    emoji.emojize("우리 오늘 하루를 기분 좋은 하루로 만들어봐요", use_aliases=True),
-                    emoji.emojize("우리 기분이 좋아지기 위해서 노래를 들어보는 건 어떨까요?", use_aliases=True),
-                    emoji.emojize("오늘 힐링이 필요하다면, 맛있는 음식을 먹어보는건 어때요? :fries: :doughnut:", use_aliases=True)
-                ]
-                res_num = random.randrange(0, len(res_list))
-                res = res_list[res_num]
+            question = "mood"
+            res = respon.mood_response(4, status)
 
         t = threading.Thread(target=data_storing.Data_Storing.data_storing(),
                              args=(question, text, pos, neg, mood, sleep_disorder, how_long, eating_disorder))
@@ -105,13 +76,7 @@ def webhook():
 
     elif action == 'input.unknown':
         text = str(req.get('queryResult').get('queryText'))
-        res_list = [
-            emoji.emojize(str(text + "라고 말씀하셨는데, \n제가 말씀을 잘 못 이해 한것 같아요. \n이해할 수 있게 다시 말씀해주시겠어요?"), use_aliases=True),
-            emoji.emojize(str(text + "라고 방금 하신 말씀은 \n제가 이해하지 못하는 말인 것 같아요. \n이해할 수 있는 쉬운 말로 다시 말씀해주시겠어요?"),
-                          use_aliases=True)
-        ]
-        res_num = random.randrange(0, len(res_list))
-        res = res_list[res_num]
+        res = respon.unknown_response(text)
 
     else:
         log.error('Unexpected action.')
--- Mood Disorder Diagnosis Chatbot/v_1 (openpyxl_use)/module/response.py
+++ Mood Disorder Diagnosis Chatbot/v_1 (openpyxl_use)/module/response.py
@@ -5,19 +5,30 @@ import random
 
 class Response:
     def welcome_response(self):
-        res_num = random.randrange(0, len(self.res_l))
-        res = self.res_l[res_num]
+        res_num = random.randrange(0, len(self.welcome_res))
+        res = self.welcome_res[res_num]
         return res
 
     def mood_response(self, context_n, status):
-        res_num = random.randrange(0, len(self.res_l))
-        res = self.res_l[context_n][status][res_num]
+        res_num = random.randrange(0, len(self.mood_response()))
+        res = self.mood_res[context_n][status][res_num]
         return res
 
     def suggestion_response(self, context_n, status):
-        res_num = random.randrange(0, len(self.res_l))
-        res = self.res_l[context_n][status][res_num]
+        res_num = random.randrange(0, len(self.sug_res))
+        res = self.sug_res[context_n][status][res_num]
         return res
+
+    def unknown_response(self, text):
+        res_list = [
+            emoji.emojize(str(text + "라고 말씀하셨는데, \n제가 말씀을 잘 못 이해 한것 같아요. \n이해할 수 있게 다시 말씀해주시겠어요?"), use_aliases=True),
+            emoji.emojize(str(text + "라고 방금 하신 말씀은 \n제가 이해하지 못하는 말인 것 같아요. \n이해할 수 있는 쉬운 말로 다시 말씀해주시겠어요?"),
+                          use_aliases=True)
+        ]
+        res_num = random.randrange(0, len(res_list))
+        res = res_list[res_num]
+        return res
+
 
     def sleep_response(self, text, morph_l):
         res = ""
@@ -117,17 +128,15 @@ class Response:
         return res, eating_disorder
 
     def __init__(self):
-        self.res_l = []
-
-        # first response (welcome)
-        self.res_l[0] = ([
+        self.welcome_res = [
             emoji.emojize("안녕하세요, 오늘 기분은 어떠신가요? ", use_aliases=True),
             emoji.emojize("안녕하세요, 오늘 하루는 어떤 일이 있었나요?", use_aliases=True),
             emoji.emojize("안녕하세요, 오늘은 어떤 하루를 보냈나요?", use_aliases=True)
-        ])
+        ]
 
+        self.mood_res = []
         # second response (mood_1)
-        self.res_l[1] = {
+        self.mood_res[1] = {
             "positive": [
                 emoji.emojize("그런가요? 기분이 좋으신 것 같아요 ", use_aliases=True),
                 emoji.emojize("그래요? 어쩐지 기분이 괜찮아 보이셨어요 ", use_aliases=True),
@@ -148,7 +157,7 @@ class Response:
         }
 
         # third response (mood_2)
-        self.res_l[2] = {
+        self.mood_res[2] = {
             "positive": [
                 emoji.emojize("기분이 좋으신 것 같아 저도 기분이 좋네요. 또 다른 일은 없었나요??", use_aliases=True),
                 emoji.emojize("좋은 하루였나봐요! 또 다른 일은 없었나요??", use_aliases=True),
@@ -169,7 +178,7 @@ class Response:
         }
 
         # fourth response (mood_3 & sleep asking)
-        self.res_l[3] = {
+        self.mood_res[3] = {
             "positive": [
                 emoji.emojize("그래요? 잠은 잘 주무시나요? 몇시간 정도 주무셨어요?", use_aliases=True),
                 emoji.emojize("그렇군요! 잠은 잘 주무시나요? 몇시간 정도 주무셨어요?:blush:", use_aliases=True)
@@ -184,6 +193,27 @@ class Response:
             "neutral": [
                 emoji.emojize("그랬군요.. 잠은 잘 주무시나요? 몇시간 정도 주무셨어요?", use_aliases=True),
                 emoji.emojize("그렇군요. 잠은 잘 주무시나요? 몇시간 정도 주무셨어요?", use_aliases=True)
+            ]
+        }
+
+        self.sug_res = {
+            "positive": [
+                emoji.emojize("오늘 하루를 더 기분좋은 하루로 만들어봐요 우리!", use_aliases=True),
+                emoji.emojize("내일도 오늘처럼 기분 좋은 하루가 되길 바래요", use_aliases=True),
+            ],
+
+            "negative": [
+                emoji.emojize("맛있는 음식을 먹으면 기분이 좀 나아지지 않을까요? :fork_and_knife: :spaghetti: :sushi:",
+                              use_aliases=True),
+                emoji.emojize("우리 신나는 음악 들어볼래요? 기분이 나아질 거에요", use_aliases=True),
+                emoji.emojize("무엇을 하면 기분이 좋아질 수 있을까요? ", use_aliases=True),
+                emoji.emojize("재미있는 영화를 보면 기분이 나아질까요? ", use_aliases=True)
+                ],
+
+            "neutral": [
+                emoji.emojize("우리 오늘 하루를 기분 좋은 하루로 만들어봐요", use_aliases=True),
+                emoji.emojize("우리 기분이 좋아지기 위해서 노래를 들어보는 건 어떨까요?", use_aliases=True),
+                emoji.emojize("오늘 힐링이 필요하다면, 맛있는 음식을 먹어보는건 어때요? :fries: :doughnut:", use_aliases=True)
             ]
         }
 
